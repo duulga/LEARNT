da1e33fc61df44af42b471429ebc2c7f077c5af8
duulga <duulgadudu@gmail.com>
2024-02-01 10:53:43 +0900

    Implement positional encoding matrix

--- train.py
+++ train.py
@@ -12,8 +12,10 @@ from Tokenizer import tokenizer, vectorizer, normalizer
 import pickle
 import random
 import tensorflow as tf
+import posenc
+import matplotlib.pyplot as plt
 
-pairs = normalizer(output_emitter(100))
+pairs = normalizer(output_emitter(150))
 with open("pairs.pickle", "wb") as fp:
     pickle.dump(pairs,fp)
 
@@ -30,15 +32,22 @@ val_pairs = bb_pairs[n_train: n_train+n_val]
 test_pairs = bb_pairs[n_train+n_val:]
 
 
-
 def format_dataset(asm, ir):
 
     source = {"encoder_inputs": asm, "decoder_inputs": ir[:, :-1]}
     target = ir[:, 2:]
     return (source, target)
 
+
+def format_dataset(asm, ir):
+
+    source = {"encoder_inputs": asm, "decoder_inputs": ir[:, :-1]}
+    #print(f'DEBUGDEBUGDEBUG {source["decoder_inputs"]}')
+    target = ir[:, 2:]
+    return (source, target)
+
 def make_dataset(pairs, batch_size=64):
-    #Create Tensorflow Dataset for the basic block pairs
+    # Create Tensorflow Dataset for the basic block pairs
     # unpack the basic blocks
     asm_tokens, ir_tokens, asm_iv_dict, ir_iv_dict, tokenized_pairs = tokenizer(pairs)
     asm_bbs = [pair[0] for pair in tokenized_pairs]
@@ -47,8 +56,10 @@ def make_dataset(pairs, batch_size=64):
     _, _, ir_vectorized = vectorizer(ir_tokens, ir_bbs)
     #vectorized = format_dataset(asm_tokens, ir_tokens, asm_bbs, ir_bbs)
     #Create tensors
+    #print(f'DEBUGDEBUG {asm_vectorized}')
+    #print(f'DEBUGDEBUG {ir_vectorized}')
     dataset = tf.data.Dataset.from_tensor_slices((asm_vectorized, ir_vectorized))
-    return dataset.shuffle(2048).batch(batch_size).map(format_dataset)
+    return dataset.shuffle(2048).batch(batch_size).map(format_dataset).prefetch(16).cache()
 
 train_ds = make_dataset(train_pairs)
 val_ds = make_dataset(val_pairs)
@@ -56,8 +67,23 @@ val_ds = make_dataset(val_pairs)
 #test the dataset
 for inputs, targets in train_ds.take(1):
     print(f'inputs["encoder_inputs"].shape: {inputs["encoder_inputs"].shape}')
-    print(f'inputs["encoder_inputs"][0]: {inputs["encoder_inputs"][0]}')
-    print(f'inputs["decoder_inputs"].shape: {inputs["decoder_inputs"].shape}')
-    print(f'inputs["decoder_inputs"][0]: {inputs["decoder_inputs"][0]}')
-    print(f"targets.shape: {targets.shape}")
-    print(f"targets[0]: {targets[0]}")
+#    print(f'inputs["decoder_inputs"].shape: {inputs["decoder_inputs"].shape}')
+#     print(f'inputs["decoder_inputs"][0]: {inputs["decoder_inputs"][0]}')
+#     print(f"targets.shape: {targets.shape}")
+#     print(f"targets[0]: {targets[0]}")
+
+
+
+
+#test the dataset
+for inputs, targets in train_ds.take(1):
+    print(inputs["encoder_inputs"])
+    print(f"its shape: {inputs['encoder_inputs'].shape}")
+    vocab_size_asm = 10000
+    seq_length = inputs['encoder_inputs'].shape[1]
+    embed_asm = posenc.PositionalEmbedding(seq_length, vocab_size_asm, embed_dim = 512)
+    asm_emb = embed_asm(inputs["encoder_inputs"])
+    print(asm_emb.shape)
+    print(asm_emb)
+
+
