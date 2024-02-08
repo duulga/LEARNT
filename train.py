870f3fbec2c878c892bc375ff637db741e992606
duulga <duulgadudu@gmail.com>
2024-02-08 16:06:48 +0900

    READY TO TRAIN

--- train.py
+++ train.py
@@ -14,6 +14,7 @@ import random
 import tensorflow as tf
 import posenc
 import matplotlib.pyplot as plt
+from transformer import transformer, masked_accuracy, masked_loss, CustomSchedule
 
 pairs = normalizer(output_emitter(150))
 with open("pairs.pickle", "wb") as fp:
@@ -65,25 +66,57 @@ train_ds = make_dataset(train_pairs)
 val_ds = make_dataset(val_pairs)
 
 #test the dataset
-for inputs, targets in train_ds.take(1):
-    print(f'inputs["encoder_inputs"].shape: {inputs["encoder_inputs"].shape}')
+# for inputs, targets in train_ds.take(1):
+#     print(f'inputs["encoder_inputs"].shape: {inputs["encoder_inputs"].shape}')
 #    print(f'inputs["decoder_inputs"].shape: {inputs["decoder_inputs"].shape}')
 #     print(f'inputs["decoder_inputs"][0]: {inputs["decoder_inputs"][0]}')
 #     print(f"targets.shape: {targets.shape}")
 #     print(f"targets[0]: {targets[0]}")
 
-
-
-
 #test the dataset
-for inputs, targets in train_ds.take(1):
-    print(inputs["encoder_inputs"])
-    print(f"its shape: {inputs['encoder_inputs'].shape}")
-    vocab_size_asm = 10000
-    seq_length = inputs['encoder_inputs'].shape[1]
-    embed_asm = posenc.PositionalEmbedding(seq_length, vocab_size_asm, embed_dim = 512)
-    asm_emb = embed_asm(inputs["encoder_inputs"])
-    print(asm_emb.shape)
-    print(asm_emb)
+# for inputs, targets in train_ds.take(1):
+#     print(inputs["encoder_inputs"])
+#     print(f"its shape: {inputs['encoder_inputs'].shape}")
+#     vocab_size_asm = 10000
+#     seq_length = inputs['encoder_inputs'].shape[1]
+#     embed_asm = posenc.PositionalEmbedding(seq_length, vocab_size_asm, embed_dim = 512)
+#     asm_emb = embed_asm(inputs["encoder_inputs"])
+#     print(asm_emb.shape)
+#     print(asm_emb)
 
+vocab_size_en = 10000
+vocab_size_fr = 20000
+seq_len = 20
+num_layers = 4
+num_heads = 8
+key_dim = 128
+ff_dim = 512
+dropout = 0.1
+vocab_size_asm = 10000
+vocab_size_fr = 20000
 
+model = transformer(num_layers, num_heads, seq_len, key_dim, ff_dim,
+                    vocab_size_en, vocab_size_fr, dropout)
+lr = CustomSchedule(key_dim)
+optimizer = tf.keras.optimizers.Adam(lr, beta_1=0.9, beta_2=0.98, epsilon=1e-9)
+model.compile(loss=masked_loss, optimizer=optimizer, metrics=[masked_accuracy])
+epochs = 20
+history = model.fit(train_ds, epochs=epochs, validation_data=val_ds)
+
+#Save the trained model
+model.save("asm-llvm-transformer.h5")
+
+# Plot the loss and accuracy history
+fig, axs = plt.subplots(2, figsize=(6, 8), sharex=True)
+fig.suptitle('Traininig history')
+x = list(range(1, epochs+1))
+axs[0].plot(x, history.history["loss"], alpha=0.5, label="loss")
+axs[0].plot(x, history.history["val_loss"], alpha=0.5, label="val_loss")
+axs[0].set_ylabel("Loss")
+axs[0].legend(loc="upper right")
+axs[1].plot(x, history.history["masked_accuracy"], alpha=0.5, label="acc")
+axs[1].plot(x, history.history["val_masked_accuracy"], alpha=0.5, label="val_acc")
+axs[1].set_ylabel("Accuracy")
+axs[1].set_xlabel("epoch")
+axs[1].legend(loc="lower right")
+plt.show()
---
+++ trainprep.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+def masked_loss(label, pred):
+    mask = label != 0
+
+    loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+        from_logits=True, reduction='none')
+    loss = loss_object(label, pred)
+
+    mask = tf.cast(mask, dtype=loss.dtype)
+    loss *= mask
+    loss = tf.reduce_sum(loss)/tf.reduce_sum(mask)
+    return loss
+
+
+def masked_accuracy(label, pred):
+    pred = tf.argmax(pred, axis=2)
+    label = tf.cast(label, pred.dtype)
+    match = label == pred
+
+    mask = label != 0
+
+    match = match & mask
+
+    match = tf.cast(match, dtype=tf.float32)
+    mask = tf.cast(mask, dtype=tf.float32)
+    return tf.reduce_sum(match)/tf.reduce_sum(mask)
+
+class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+    "Custom learning rate for Adam optimizer"
+    def __init__(self, key_dim, warmup_steps=4000):
+        super().__init__()
+        self.key_dim = key_dim
+        self.warmup_steps = warmup_steps
+        self.d = tf.cast(self.key_dim, tf.float32)
+
+    def __call__(self, step):
+        step = tf.cast(step, dtype=tf.float32)
+        arg1 = tf.math.rsqrt(step)
+        arg2 = step * (self.warmup_steps ** -1.5)
+        return tf.math.rsqrt(self.d) * tf.math.minimum(arg1, arg2)
+
+    def get_config(self):
+        # to make save and load a model using custom layer possible0
+        config = {
+            "key_dim": self.key_dim,
+            "warmup_steps": self.warmup_steps,
+        }
+        return config
+
+key_dim = 128
+lr = CustomSchedule(key_dim)
+optimizer = tf.keras.optimizers.Adam(lr, beta_1=0.9, beta_2=0.98, epsilon=1e-9)
+
+plt.plot(lr(tf.range(50000, dtype=tf.float32)))
+plt.ylabel('Learning Rate')
+plt.xlabel('Train Step')
+plt.show()
